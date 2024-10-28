fcebaf0ff5016379c09cab00f18d02060e387063
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-10-28 11:02:53 -0300

    feat: adiciona mensagem de feedback ao criar o produto

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,9 +1,12 @@
 import { Component, inject } from '@angular/core';
+import { ProductsService } from '../../shared/services/products.service';
+import { Router } from '@angular/router';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
 import { MatButtonModule } from '@angular/material/button'
-import { ProductsService } from '../../shared/services/products.service';
+import { MatSnackBar } from '@angular/material/snack-bar'
 
 @Component({
   selector: 'app-create',
@@ -19,6 +22,8 @@ import { ProductsService } from '../../shared/services/products.service';
 })
 export class CreateComponent {
   productService = inject(ProductsService);
+  matSnackBar = inject(MatSnackBar)
+  router = inject(Router);
 
   form = new FormGroup({
     title: new FormControl<string>('', {
@@ -32,7 +37,12 @@ export class CreateComponent {
       title: this.form.controls.title.value
     })
       .subscribe(() => {
-        alert('Sucesso!');
-      })
+        this.matSnackBar.open('Produto criado com sucesso!', 'OK', {
+          duration: 3000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top'
+        });
+        this.router.navigateByUrl('/')
+      });
   }
 }
