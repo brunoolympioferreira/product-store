348389bd0c9e2785c3ff4e8a993446603adfbaba
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-10-29 14:22:21 -0300

    feat: adiciona pagina de edicao

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
-import { CreateComponent } from './features/create/create.component';
 
 export const routes: Routes = [
     {
@@ -13,5 +12,12 @@ export const routes: Routes = [
             import('./features/create/create.component').then(
                 (m) => m.CreateComponent
             ),
+    },
+    {
+        path: 'edit-product',
+        loadComponent: () =>
+            import('./features/edit/edit.component').then(
+                (m) => m.EditComponent
+            )
     },
 ];
