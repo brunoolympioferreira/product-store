581a556cf50d14fd55cc0f14b37c325797476610
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-10-30 17:47:15 -0300

    feat: adiciona funcionalidade de edicao

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
-import { Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
+import { inject } from '@angular/core';
+import { ProductsService } from './shared/services/products.service';
 
 export const routes: Routes = [
     {
@@ -14,10 +16,14 @@ export const routes: Routes = [
             ),
     },
     {
-        path: 'edit-product',
+        path: 'edit-product/:id',
+        resolve: {
+            product: (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+                const productsService = inject(ProductsService)
+                return productsService.get(route.paramMap.get('id') as string);
+            },
+        },
         loadComponent: () =>
-            import('./features/edit/edit.component').then(
-                (m) => m.EditComponent
-            )
+            import('./features/edit/edit.component').then((m) => m.EditComponent),
     },
 ];
