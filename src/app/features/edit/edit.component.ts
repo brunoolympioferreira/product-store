84beab36f7c20e8801e905bfb6dfc5a013e65e73
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-11-12 13:59:18 -0300

    feat: adiciona back to list component

--- src/app/features/edit/edit.component.ts
+++ src/app/features/edit/edit.component.ts
@@ -4,12 +4,14 @@ import { ProductsService } from '../../shared/services/products.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormComponent } from '../../shared/components/form/form.component';
+import { BackToListComponent } from '../../shared/components/back-to-list/back-to-list.component';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
   imports: [
-    FormComponent
+    FormComponent,
+    BackToListComponent
   ],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss'
